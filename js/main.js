a7473b2194e043e240bc726ba07277265a734c07
SpongeCoder <nostro71@yandex.ru>
2015-11-30 22:52:13 +0300

    Добавлено освещение, - пока еще корявое Добавлена пауза

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
 				click			= false,
 				mouseY 		= 0,
 				mouseX 		= 0,
+				pause			= false,
 				sceneSize = {w: window.innerWidth, h: window.innerHeight},
 				scene 		= new THREE.Scene(),
 				camera 		= new THREE.PerspectiveCamera( 75, sceneSize.w / sceneSize.h, 1, 10000 ),
@@ -14,8 +15,7 @@
 				controls;
 
 		var init = function () {
-			renderer.setSize( sceneSize.w, sceneSize.h );
-			document.body.appendChild( renderer.domElement );
+			
 
 
 			controls = new THREE.OrbitControls( camera );
@@ -23,10 +23,28 @@
 			controls.addEventListener( 'change', render );
 
 
+			renderer.setSize( sceneSize.w, sceneSize.h );
+
+			// renderer.shadowMap.enabled = true;
+			// renderer.shadowMap.type = THREE.BasicShadowMap;
+
+			document.body.appendChild( renderer.domElement );
+
 			// Events
-			document.addEventListener('mousemove', function(event) {
+			document.addEventListener('mousemove', function (event) {
 				mouseY = parseInt(event.offsetY);
 				mouseX = parseInt(event.offsetX);
+			});
+
+			document.addEventListener('keyup', function (event) {
+				console.log(event.keyCode);
+				// spacebar
+				if (event.keyCode == 32)
+				{
+					if (pause) pause = false;
+					else pause = true;
+					// pause = pause || true;
+				}
 			});
 
 			/*document.addEventListener('mousedown', function(event) {
@@ -73,6 +91,22 @@
 		var neptuneOrbit = new Orbit(370);
 		neptuneOrbit.draw(scene);
 
+		sun.material.emissive.set(0xCAAA33); // read doc
+		sun.material.emissiveMap = sun.material.map; // read doc
+
+		light = new THREE.PointLight(0xfcd440, 2, 8000);
+		light.castShadow = true;
+		light.shadowCameraNear = 1;
+		light.shadowCameraFar = 30;
+		light.shadowCameraVisible = true;
+		light.shadowMapWidth = 2048;
+		light.shadowMapHeight = 1024;
+		light.shadowBias = 0.01;
+		light.shadowDarkness = 0.5;
+
+
+    sun.add(light);
+
 		scene.add(sun);
 		scene.add(mercury);
 		scene.add(venus);
@@ -82,7 +116,18 @@
 		scene.add(saturn);
 		scene.add(uranus);
 		scene.add(neptune);
-		scene.add( new THREE.AmbientLight( 0xffffff ) ); // light
+
+
+				
+
+
+		// var light = new THREE.PointLight( 0xffffbb, 1.5, 10000, 1 );
+		// light.position.set( 0, 0, 0 );
+		// light.castShadow = true;
+
+
+		var ambient = new THREE.AmbientLight(0x404040);
+    scene.add(ambient);
 
 		camera.position.z = CAMERA_POSITION;
 		camera.position.y = CAMERA_POSITION / 2;
@@ -90,43 +135,50 @@
 		var t = 0;
 
 		var update = function() {
+
+
 			sun.rotation.y += 0.001;
-			mercury.rotation.y += 0.01;
-			venus.rotation.y += 0.02;
-			earth.rotation.y += 0.015;
-			mars.rotation.y += 0.01;
-			jupiter.rotation.y += 0.01;
-			saturn.rotation.y += 0.01;
-			uranus.rotation.y += 0.01;
-			neptune.rotation.y += 0.01;
-
-			mercury.position.x = Math.sin(t*0.25)*65;
-			mercury.position.z = Math.cos(t*0.25)*65;
-
-			venus.position.x = Math.sin(t*0.2)*80;
-			venus.position.z = Math.cos(t*0.2)*80;
-
-			earth.position.x = Math.sin(t*0.3)*120;
-			earth.position.z = Math.cos(t*0.3)*120;
-
-			mars.position.x = Math.sin(t*0.2)*140;
-			mars.position.z = Math.cos(t*0.2)*140;
-
-			jupiter.position.x = Math.sin(t*0.15)*200;
-			jupiter.position.z = Math.cos(t*0.15)*200;
-
-			saturn.position.x = Math.sin(t*0.12)*280;
-			saturn.position.z = Math.cos(t*0.12)*280;
-
-			uranus.position.x = Math.sin(t*0.05)*340;
-			uranus.position.z = Math.cos(t*0.05)*340;
-
-			neptune.position.x = Math.sin(t*0.02)*370;
-			neptune.position.z = Math.cos(t*0.02)*370;
+				mercury.rotation.y += 0.01;
+				venus.rotation.y += 0.02;
+				earth.rotation.y += 0.015;
+				mars.rotation.y += 0.01;
+				jupiter.rotation.y += 0.01;
+				saturn.rotation.y += 0.01;
+				uranus.rotation.y += 0.01;
+				neptune.rotation.y += 0.01;
 			
-			//camera.lookAt(scene.position);
+			if (!pause)
+			{
+				
 
-			t += Math.PI / 180 * 2;
+				mercury.position.x = Math.sin(t*0.25)*65;
+				mercury.position.z = Math.cos(t*0.25)*65;
+
+				venus.position.x = Math.sin(t*0.2)*80;
+				venus.position.z = Math.cos(t*0.2)*80;
+
+				earth.position.x = Math.sin(t*0.3)*120;
+				earth.position.z = Math.cos(t*0.3)*120;
+
+				mars.position.x = Math.sin(t*0.2)*140;
+				mars.position.z = Math.cos(t*0.2)*140;
+
+				jupiter.position.x = Math.sin(t*0.15)*200;
+				jupiter.position.z = Math.cos(t*0.15)*200;
+
+				saturn.position.x = Math.sin(t*0.12)*280;
+				saturn.position.z = Math.cos(t*0.12)*280;
+
+				uranus.position.x = Math.sin(t*0.05)*340;
+				uranus.position.z = Math.cos(t*0.05)*340;
+
+				neptune.position.x = Math.sin(t*0.02)*370;
+				neptune.position.z = Math.cos(t*0.02)*370;
+				
+				//camera.lookAt(scene.position);
+
+				t += Math.PI / 180 * 2;
+			}
 		}
 
 		var loop = function() {
@@ -149,22 +201,33 @@
 	function planet (radius, wSeg,hSeg, textureUrl) {
 		var planet, geometry, material, texture;
 
-		texture = THREE.ImageUtils.loadTexture(textureUrl);
-		texture.anisotropy = 8;
+		var loader = new THREE.TextureLoader();
+
+		texture = loader.load(textureUrl);
+		texture.anisotropy = 100;
 
 		geometry 	= new THREE.SphereGeometry(radius, wSeg, hSeg);
-		material 	= new THREE.MeshPhongMaterial({ map: texture});
+		material 	= new THREE.MeshPhongMaterial( {map: texture} );
 		planet 		= new THREE.Mesh(geometry, material);
+
+		planet.castShadow = true;
+		// planet.receiveShadow = true;
 
 		return planet;
 	}
 
+
+	// переписать, добавив к планетам
 	var Orbit = function (radius) {
 		this.radius = radius;
 
 		this.draw = function (scene) {
 			var geometry = new THREE.Geometry();
-			var material = new THREE.ParticleBasicMaterial({color: 0xbbbbbb, size: 1, sizeAttenuation: false});
+			var material = new THREE.PointsMaterial({
+				color: 0xbbbbbb,
+				size: 1,
+				sizeAttenuation: false
+			});
 
 			for (var i = 0; i < 500; i++) {
 				var vertex = new THREE.Vector3();
@@ -174,7 +237,7 @@
 
 				geometry.vertices.push(vertex);
 			}
-			var orbita = new THREE.ParticleSystem(geometry, material);
+			var orbita = new THREE.Points(geometry, material);
 			scene.add(orbita);
 		}
 
